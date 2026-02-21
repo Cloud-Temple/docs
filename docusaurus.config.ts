668dd456ce0d869a48fe42f3166c402b12b1542b
christophe.lesur <christophe.lesur@cloud-temple.com>
2026-02-21 16:37:32 +0100

    🐛 Fix stray character in docusaurus.config.ts (ParseError)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,4 +1,4 @@
-eimport { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import remarkMath from 'remark-math';
