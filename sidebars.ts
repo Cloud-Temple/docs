2414f08f1c978ef1807e49e7600b43999d4935be
kcn <kevin.chapron@cloud-temple.com>
2026-04-16 18:59:12 +0200

    Update IaaS to Compute

--- sidebars.ts
+++ sidebars.ts
@@ -152,7 +152,7 @@ const sidebars: SidebarsConfig = {
     },
     {
       type: 'category',
-      label: 'IaaS',
+      label: 'Compute',
       className: 'sidebar-cat-compute',
       link: { type: 'doc', id: 'compute_overview' },
       items: [
@@ -169,7 +169,7 @@ const sidebars: SidebarsConfig = {
         },
         {
           type: 'category',
-          label: 'OpenSource',
+          label: 'IaaS OpenSource',
           link: { type: 'doc', id: 'iaas_opensource/iaas_opensource' },
           items: [
             'iaas_opensource/iaas_opensource',
@@ -206,7 +206,7 @@ const sidebars: SidebarsConfig = {
         },
         {
           type: 'category',
-          label: 'VMware',
+          label: 'IaaS VMware',
           link: { type: 'doc', id: 'iaas_vmware/iaas_vmware' },
           items: [
             'iaas_vmware/iaas_vmware',
